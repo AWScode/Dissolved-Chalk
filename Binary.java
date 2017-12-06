a6d4f25711e9101f1d1b59f2b95350c0a4429838
Rachel <rachel_holland@aw.org>
2017-12-06 13:24:09 -0800

    hi

---
+++ Binary.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class Binary
+  public static void main(String[] args) {
+    System.out.println("Hello! Welcome to java Binary!");
+    System.out.println("What is your first binary number?");
+    Scanner input1 = new Scanner(System.in);
+    Integer theirInput1 = input1.nextInt();
+    ArrayList first = new ArrayList(8);
+    for(i;
+  }
